12f2a0189900d6df88dcff8cf690e330d62b53d9
Tolmachev Dmitrii <dtolm@yandex.ru>
2020-11-14 05:34:03 +0100

    Switch license from LGPL to MPL2

--- lib/vkFFT.h
+++ lib/vkFFT.h
@@ -1,3 +1,10 @@
+// This file is part of VkFFT, a Vulkan Fast Fourier Transform library
+//
+// Copyright (C) 2020 Dmitrii Tolmachev <dtolm96@gmail.com>
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/. 
 #ifdef __cplusplus
 extern "C" {
 #endif
